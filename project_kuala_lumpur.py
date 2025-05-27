6574787254aa51f21ce9cdd7b9e714acccf555c8
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2025-05-27 16:35:08 +0530

    new changes

--- project_kuala_lumpur.py
+++ project_kuala_lumpur.py
@@ -26,10 +26,17 @@ try:
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     temp_profile_dir = tempfile.mkdtemp()
-    
     options = Options()
+    
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--start-maximized")
     options.add_argument(f"--user-data-dir={temp_profile_dir}")
 
     driver = webdriver.Chrome(options=options)
